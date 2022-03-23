a683197d6c114e6804e11c4c74475aa712544b94
Yaser Shaikh <yasersme@gmail.com>
2022-03-23 07:23:21 +0530

    First user interactive mini project

---
+++ proj1.py
@@ -0,0 +1,6 @@
+# Project 1 - Band Name Generator
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be ",city,pet_name)
