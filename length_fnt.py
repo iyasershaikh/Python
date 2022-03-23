cfa842cd31fc02e71e163f7087b5d91751fa64d0
Yaser Shaikh <yasersme@gmail.com>
2022-03-23 07:22:24 +0530

    Here we learnt the len function

---
+++ length_fnt.py
@@ -0,0 +1,8 @@
+# Here we are learning the len() function to find the length of a string
+name = input("What is your name? \n")
+length = len(name)
+print("The length of the your name is: ", length)
+
+# Here we do the same code but with shortest possible code length
+print("The length of your name is: ", len(input("What is Your Name?\n")))
+# But here in this code we don't store any values
